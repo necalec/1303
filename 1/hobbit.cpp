c1da15332a8444bec19f5b4d3af78ff90f161b04
Nikita Zhukov <necalec@gmail.com>
2024-04-16 00:37:55 +0300

    main

---
+++ 1/hobbit.cpp
@@ -0,0 +1,67 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <fstream> 
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <omp.h>
+#include <chrono>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    unordered_map<string, int> hash;
+    string line;
+    string delimiters = " ,-!>[]<{};:.?/|_+=*&!?- -";
+    char* token;
+
+    ifstream in("text.txt");
+    if (in.is_open())
+    {
+        auto start_time = chrono::high_resolution_clock::now();
+        #pragma omp parallel shared(hash)
+        while (getline(in, line))
+        {
+            #pragma omp single nowait
+            char* cstr = new char[line.length() + 1];
+            strcpy(cstr, line.c_str());
+
+            token = strtok(cstr, delimiters.c_str());
+            while (token != NULL)
+            {
+                string word(token);
+                ++hash[word];
+                token = strtok(NULL, delimiters.c_str());
+            }
+            delete[] cstr;
+        }
+        in.close();
+        auto end_time = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
+        cout << duration.count() << endl;
+    }
+    else
+    {
+        cout << "no" << endl;
+        return 1;
+    }
+
+    vector<pair<string, int>> sortedByFreq(hash.begin(), hash.end());
+
+    auto cmp = [](const pair<string, int>& a, const pair<string, int>& b) {
+        return a.second > b.second;
+        };
+
+    sort(sortedByFreq.begin(), sortedByFreq.end(), cmp);
+
+    for (const auto& pair : sortedByFreq) {
+        std::cout << pair.first << " " << pair.second << "\n";
+    }
+
+    return 0;
+}
---
+++ 2/life.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <Windows.h>
+
+using namespace std;
+
+class GameOfLife {
+private:
+    int rows;
+    int cols;
+    vector<vector<bool>> board;
+
+public:
+    GameOfLife(int rows, int cols) : rows(rows), cols(cols), board(rows, vector<bool>(cols, false)) {}
+
+    void randomize() {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                board[i][j] = rand() % 2 == 1;
+            }
+        }
+    }
+
+    void draw() {
+        system("cls");
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                cout << (board[i][j] ? 'Я' : ' ');
+            }
+            cout << endl;
+        }
+    }
+
+    void update() {
+        vector<vector<bool>> new_board(rows, vector<bool>(cols, false));
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                int neighbors = count_neighbors(i, j);
+
+                if (board[i][j]) {
+                    new_board[i][j] = (neighbors == 2 || neighbors == 3);
+                }
+                else {
+                    new_board[i][j] = (neighbors == 3);
+                }
+            }
+        }
+
+        board = new_board;
+    }
+
+private:
+    int count_neighbors(int i, int j) {
+        int neighbors = 0;
+        for (int di = -1; di <= 1; ++di) {
+            for (int dj = -1; dj <= 1; ++dj) {
+                if (di == 0 && dj == 0) continue;
+                int ni = i + di;
+                int nj = j + dj;
+                if (ni >= 0 && ni < rows && nj >= 0 && nj < cols && board[ni][nj]) {
+                    ++neighbors;
+                }
+            }
+        }
+        return neighbors;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    const int rows = 20;
+    const int cols = 60;
+
+    GameOfLife game(rows, cols);
+
+    game.randomize();
+
+    for (int generation = 0; generation < 100; ++generation) {
+        game.draw();
+        Sleep(200);
+        game.update();
+    }
+
+    return 0;
+}
+
+
+
